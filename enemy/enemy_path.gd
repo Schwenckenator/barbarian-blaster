8250c6d887f3d24e0017629898f3d2ca6e40fdb3
Matthew Schwencke <matt.schwencke@proton.me>
2024-07-12 23:22:58 +0900

    Added Victory Screen

--- enemy/enemy_path.gd
+++ enemy/enemy_path.gd
@@ -2,6 +2,7 @@ extends Path3D
 
 @export var enemy_scene: PackedScene
 @export var difficulty_manager: Node
+@export var victory_layer: CanvasLayer
 
 @onready var spawn_timer: Timer = $SpawnTimer
 
@@ -10,8 +11,18 @@ func spawn_enemy() -> void:
 	new_enemy.max_health = difficulty_manager.get_enemy_health()
 	add_child(new_enemy)
 	spawn_timer.wait_time = difficulty_manager.get_spawn_time()
-	print(new_enemy.health)
+	new_enemy.tree_exited.connect(enemy_defeated)
 
 
 func _on_difficulty_manager_stop_spawning_enemies() -> void:
 	spawn_timer.stop()
+
+func enemy_defeated() -> void:
+	if !spawn_timer.is_stopped():
+		return
+	
+	for child in get_children():
+		if child is PathFollow3D:
+			return
+
+	victory_layer.visible = true
